37ed31a0c8fbf400c5083a3657af41cf3e8dbc83
Carlos Peñas <carlos@bebanjo.com>
2023-07-06 12:39:02 +0200

    fix amifind

--- bin/amifind.rb
+++ bin/amifind.rb
@@ -1,14 +1,13 @@
 #!/usr/bin/env ruby
+
+require "json"
+
 res = `curl -s 'https://cloud-images.ubuntu.com/locator/ec2/releasesTable' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36' -H 'Accept: application/json, text/javascript, */*' -H 'Referer: https://cloud-images.ubuntu.com/locator/ec2/' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive'`
 
-rows = res.split(/\n/)
+arr = JSON.parse(res)["aaData"]
+arr.map! {|x| x.join(",") }
+arr.map! { |x| x.gsub("</a>", "")}
+arr.map! { |x| x.gsub(/,<a .*>/, ",")}
 
-rows.pop
-rows.pop
 
-rows.shift
-rows.shift
-
-clean = rows.map { |x| x.gsub(/<.[^>]+>/,"").gsub(/"/,"").gsub(/,$/,"").gsub(/\[|\]/,"") }
-
-puts clean.join("\n")
+puts arr.join("\n")
